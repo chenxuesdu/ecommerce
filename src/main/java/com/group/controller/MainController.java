cc34ac9eecf975cf4563d64a5bd6bc7ca50ba9f8
chenxuesdu <chenxuesdu@gmail.com>
2017-04-07 21:10:13 -0700

    change the yaml file location

--- src/main/java/com/group/controller/MainController.java
+++ src/main/java/com/group/controller/MainController.java
@@ -17,7 +17,7 @@ public class MainController {
 	public List<String> ec2List;
 	public ELBv2Clients elbClients;
 	String elbState;
-	private static org.apache.log4j.Logger log = Logger.getLogger(MainController.class);
+	private static Logger log = Logger.getLogger(MainController.class);
 
 	@RequestMapping("/")
 	public String home() {
@@ -26,7 +26,7 @@ public class MainController {
 		return "Welcome to our ecommerce system! Weiyu";
 	}
 
-	@RequestMapping(value ="/createelb", method = RequestMethod.POST)
+	@RequestMapping(value = "/createelb", method = RequestMethod.POST)
 	public Map<String, String> createELB() {
 		final String PENDING_STATUS = "pending";
 		final String ACTIVE_STATUS = "active";
@@ -35,8 +35,10 @@ public class MainController {
 		Map<String, String> ret = new HashMap<>();
 
 		if (!ELBExist) {
-			String ec2ConfigFilePath = "/Users/wtang/Documents/295/ecommerce/src/main/java/com/group/controller/ec2Config.yaml";
-			String elbConfigFilePath = "/Users/wtang/Documents/295/ecommerce/src/main/java/com/group/controller/elbConfig.yaml";
+			String ec2ConfigFilePath = this.getClass().getClassLoader()
+					.getResource("ec2Config.yaml").toString();
+			String elbConfigFilePath = this.getClass().getClassLoader()
+					.getResource("elbConfig.yaml").toString();
 
 			elbClients = new ELBv2Clients(elbConfigFilePath);
 
@@ -54,54 +56,65 @@ public class MainController {
 						ec2Instance.ec2Config.getSecurityGroupId(),
 						subnetIdList.get(i % 2));
 
-				System.out.println("The newly created ec2 instance has an ID: " + instanceId);
+				System.out.println("The newly created ec2 instance has an ID: "
+						+ instanceId);
 				System.out.println(ec2Instance.getInstanceId());
 				System.out.println("**********************");
 			}
 
 			try {
-				System.out.println("Sleep 20 seconds starting all EC2 instances.");
-				Thread.sleep(20000);    // Wait until all ec2 instances are up.
+				System.out
+						.println("Sleep 20 seconds starting all EC2 instances.");
+				Thread.sleep(20000); // Wait until all ec2 instances are up.
 			} catch (Exception e) {
 				System.out.print(e);
 			}
 
 			ec2List = ec2Instance.listInstances(RUNNING_STATUS);
 
-			//List<String> ec2List = ec2Instance.listInstances(STOPPED_STATUS);
+			// List<String> ec2List = ec2Instance.listInstances(STOPPED_STATUS);
 
 			for (String inst : ec2List) {
 				ec2Instance.startInstance(inst);
-				//ec2Instance.stopInstance(inst);
+				// ec2Instance.stopInstance(inst);
 			}
 
 			try {
-				Thread.sleep(20000);    // Wait until all ec2 instances are up.
+				Thread.sleep(20000); // Wait until all ec2 instances are up.
 			} catch (Exception e) {
 				System.out.print(e);
 			}
 
-			List<String> ec2InstanceSubnetIds = ec2Instance.listInstanceSubnetIds(RUNNING_STATUS);
-			for (String s : ec2InstanceSubnetIds) System.out.println(s);
+			List<String> ec2InstanceSubnetIds = ec2Instance
+					.listInstanceSubnetIds(RUNNING_STATUS);
+			for (String s : ec2InstanceSubnetIds)
+				System.out.println(s);
 
-			List<SecurityGroup> securityGroups = ec2Instance.getSecurityGroupId(elbClients.elbConfig.getSecurityGroupName());
+			List<SecurityGroup> securityGroups = ec2Instance
+					.getSecurityGroupId(elbClients.elbConfig
+							.getSecurityGroupName());
 			List<String> securityGroupId = new ArrayList<>();
 			for (SecurityGroup s : securityGroups) {
 				securityGroupId.add(s.getGroupId());
 			}
 
-			List<String> ec2InstanceVpcIds = ec2Instance.listInstanceVpcIds(RUNNING_STATUS);
-			for (String s : ec2InstanceVpcIds) System.out.println(s);
+			List<String> ec2InstanceVpcIds = ec2Instance
+					.listInstanceVpcIds(RUNNING_STATUS);
+			for (String s : ec2InstanceVpcIds)
+				System.out.println(s);
 
-			List<String> ec2RunningInstances = ec2Instance.listInstances(RUNNING_STATUS);
+			List<String> ec2RunningInstances = ec2Instance
+					.listInstances(RUNNING_STATUS);
 
-			elbState = elbClients.createELB(elbClients.elbConfig.getName(), securityGroupId, ec2InstanceSubnetIds, ec2RunningInstances);
+			elbState = elbClients.createELB(elbClients.elbConfig.getName(),
+					securityGroupId, ec2InstanceSubnetIds, ec2RunningInstances);
 
 			int i = 0;
 			while (!elbState.equals(ACTIVE_STATUS) && i++ < 10) {
 				try {
-					System.out.println("Sleep 20 seconds waiting ELB becoming Active.");
-					Thread.sleep(20000);    // Wait until all ec2 instances are up.
+					System.out
+							.println("Sleep 20 seconds waiting ELB becoming Active.");
+					Thread.sleep(20000); // Wait until all ec2 instances are up.
 				} catch (Exception e) {
 					System.out.println(e);
 				}
@@ -119,6 +132,5 @@ public class MainController {
 		ret.put("ELBState", elbState);
 		return ret;
 	}
-
 
 }
