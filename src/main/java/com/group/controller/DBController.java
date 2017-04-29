71835d7430422bcf3e1786c3ec954287732519b4
chenxuesdu <chenxuesdu@gmail.com>
2017-04-29 14:42:37 -0700

    update Product

--- src/main/java/com/group/controller/DBController.java
+++ src/main/java/com/group/controller/DBController.java
@@ -19,22 +19,56 @@ public class DBController {
 	@Autowired
 	private ProductRepository productRepository;
 
+	/**
+	 * Find a product by product name
+	 * 
+	 * @param name
+	 * @return
+	 */
 	@RequestMapping(value = "/product/{name}", method = RequestMethod.GET, produces = "application/json")
 	public Product getProduct(@PathVariable String name) {
-		Product product = productRepository.findByName(name);
+		Product product = this.productRepository.findByName(name);
 		return product;
 	}
 
+	/**
+	 * List all products
+	 * 
+	 * @return
+	 */
 	@RequestMapping(value = "/product/all", method = RequestMethod.GET, produces = "application/json")
 	public List<Product> getAllProduct() {
-		List<Product> list = productRepository.findAll();
+		List<Product> list = this.productRepository.findAll();
 		return list;
 	}
 
+	/**
+	 * save a product, if product name exists, update the product.
+	 * 
+	 * @param product
+	 * @return
+	 */
 	@RequestMapping(value = "/product/save", method = RequestMethod.POST, consumes = "application/json")
 	public int save(@RequestBody Product product) {
-		productRepository.save(product);
+		Product result = this.getProduct(product.name);
+		if (result == null) {
+			this.productRepository.save(product);
+		} else {
+			result.inventory = product.inventory;
+			result.price = product.price;
+			this.productRepository.save(result);
+		}
 		return 0;
+	}
+
+	/**
+	 * delete a product by id
+	 * 
+	 * @param id
+	 */
+	@RequestMapping(value = "/product/delete/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable String id) {
+		this.productRepository.delete(id);
 	}
 
 }
